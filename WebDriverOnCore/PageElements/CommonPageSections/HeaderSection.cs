06ade223793b10353624d207508f3a3ea0e9e1ff
Teltov, Kostiantyn (ext) <kostiantyn.teltov.ext@dematic.com>
2018-08-12 14:44:52 +0300

    Carousel navigation tests were added.

--- WebDriverOnCore/PageElements/CommonPageSections/HeaderSection.cs
+++ WebDriverOnCore/PageElements/CommonPageSections/HeaderSection.cs
@@ -1,4 +1,6 @@
 ﻿using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.Linq;
 using WebDriverOnCore.WebDriver;
 
 namespace WebDriverOnCore.PageSections.MainPageSections
@@ -8,6 +10,8 @@ namespace WebDriverOnCore.PageSections.MainPageSections
         public IWebElement HeaderMain => DriverInitialize.Browser.FindElement(By.ClassName("header-wrap"));
 
         public IWebElement HeaderTopBanner => DriverInitialize.Browser.FindElement(By.Id("ctl00_topBanner"));
+
+        public List<IWebElement> HeaderNavigationMenuItems => DriverInitialize.Browser.FindElements(By.CssSelector(".header-nav > ul >li")).ToList();
 
 
     }
--- WebDriverOnCore/TestsInit/TestSettings.cs
+++ WebDriverOnCore/TestsInit/TestSettings.cs
@@ -7,5 +7,18 @@ namespace WebDriverOnCore.TestsInit
         public static string GetBrowser => TestContext.Parameters["browser"];
 
         public static string BaseUrl => TestContext.Parameters["baseUrl"];
+
+        public static string NewsUrl => BaseUrl + TestContext.Parameters["newsPage"];
+
+        public static string ScoreBoardUrl => BaseUrl + TestContext.Parameters["scoreboard"];
+
+        public static string VideoUrl => BaseUrl + TestContext.Parameters["videoPage"];
+
+        public static string PhotoUrl => BaseUrl + TestContext.Parameters["photoGaleryPage"];
+
+        public static string TvScheduleUrl => BaseUrl + TestContext.Parameters["tvschedule"];
+
+        public static string ConferencesUrl => BaseUrl + TestContext.Parameters["conferences"];
+
     }
 }
